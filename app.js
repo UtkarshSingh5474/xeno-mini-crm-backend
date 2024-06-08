3339b050ae46c0c4bbf00b17c73ca5c735a5dfee
Utkarsh Singh <utkarshsingh.5474@gmail.com>
2024-06-08 23:19:12 +0530

    chore: Update app.js to use dynamic port number

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/api', testRoutes); // Use the test routes
 
 const startServer = async () => {
   await connectDB();
-  app.listen(5000, () => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log('Server is running on port 5000');
   });
 };
